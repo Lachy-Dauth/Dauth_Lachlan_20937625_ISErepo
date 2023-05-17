cb52ea42f56419f8207a243765dbf9c520d774c0
Lachlan Dauth <174690@SCT-lachlan.dauth-174690>
2023-05-17 09:26:51 +0800

    added function to convert a string into a month code

---
+++ code/season_finder.py
@@ -0,0 +1,50 @@
+def month_to_code(month_str):
+    """
+    Converts a month string to its corresponding numerical code.
+
+    Parameters:
+    month_str (str): A string representing a month, e.g. "January", "feb".
+
+    Returns:
+    int: The numerical code for the month, e.g. 1 for "January", 2 for "February", etc.
+
+    Raises:
+    ValueError: If month_str is not a valid month format.
+    """
+
+    # A dictionary of month formats and their corresponding numerical codes.
+    month_formats = {
+        "jan": 1,
+        "january": 1,
+        "feb": 2,
+        "february": 2,
+        "mar": 3,
+        "march": 3,
+        "apr": 4,
+        "april": 4,
+        "may": 5,
+        "jun": 6,
+        "june": 6,
+        "jul": 7,
+        "july": 7,
+        "aug": 8,
+        "august": 8,
+        "sep": 9,
+        "sept": 9,
+        "september": 9,
+        "oct": 10,
+        "october": 10,
+        "nov": 11,
+        "november": 11,
+        "dec": 12,
+        "december": 12
+    }
+
+    # Convert month_str to lowercase for case-insensitive matching.
+    month_str = month_str.lower()
+
+    # Check if month_str is a valid month format.
+    if month_str in month_formats:
+        return month_formats[month_str]
+    else:
+        raise ValueError(f"Invalid month format: {month_str}")
